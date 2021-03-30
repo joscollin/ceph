3b5100434cbb3ff1ca298de375e5f5d7dc19d3dd
Jos Collin <jcollin@redhat.com>
2021-03-30 17:05:15 +0530

    fixed review comments
    Signed-off-by: Jos Collin <jcollin@redhat.com>

--- src/msg/async/Protocol.h
+++ src/msg/async/Protocol.h
@@ -106,7 +106,7 @@ protected:
   AsyncConnection *connection;
   AsyncMessenger *messenger;
   CephContext *cct;
-  ceph::mono_time throttle_prev = ceph::mono_clock::zero();
+  ceph::coarse_mono_time throttle_prev = ceph::coarse_mono_clock::zero();
 public:
   std::shared_ptr<AuthConnectionMeta> auth_meta;
 
