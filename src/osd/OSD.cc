5941d2236d4b02a0bb1b81bdb5b7bf075f50adfa
Jos Collin <jcollin@redhat.com>
2021-04-22 18:05:00 +0530

    WIP - health metrics
    Signed-off-by: Jos Collin <jcollin@redhat.com>

--- src/osd/OSD.cc
+++ src/osd/OSD.cc
@@ -7812,6 +7812,7 @@ vector<DaemonHealthMetric> OSD::get_health_metrics()
     }
     metrics.emplace_back(daemon_metric::PENDING_CREATING_PGS, n_primaries);
   }
+  monc->get_health_metrics(metrics);
   return metrics;
 }
 
