7be67e13a9e2ae4a71ff2e0bb90eece4dc84432e
Adam King <adking@redhat.com>
2023-06-19 16:07:31 -0400

    mgr/cephadm: add extra_entrypoint_args to mon spec
    There was no reason for the mon spec to not include
    this option. I believe this was just an oversight caused
    by the addition of the mon spec and extra_entrypoint_args
    in separate PRs around the same time.
    
    Signed-off-by: Adam King <adking@redhat.com>

--- src/python-common/ceph/deployment/service_spec.py
+++ src/python-common/ceph/deployment/service_spec.py
@@ -1753,6 +1753,7 @@ class MONSpec(ServiceSpec):
                  preview_only: bool = False,
                  networks: Optional[List[str]] = None,
                  extra_container_args: Optional[GeneralArgList] = None,
+                 extra_entrypoint_args: Optional[GeneralArgList] = None,
                  custom_configs: Optional[List[CustomConfig]] = None,
                  crush_locations: Optional[Dict[str, List[str]]] = None,
                  ):
@@ -1765,6 +1766,7 @@ class MONSpec(ServiceSpec):
                                       preview_only=preview_only,
                                       networks=networks,
                                       extra_container_args=extra_container_args,
+                                      extra_entrypoint_args=extra_entrypoint_args,
                                       custom_configs=custom_configs)
 
         self.crush_locations = crush_locations
